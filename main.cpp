ddd7936326ed5bc221b65137db748cbd4780d53c
Hilary Cheng <hilarycheng@gmail.com>
2012-08-23 14:12:49 +0800

    - Remove Debug Message - Add dump_buffer to dump content of the request

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include "spdy_common.h"
 #include <list>
+#include <string.h>
+#include <errno.h>
 #include <ev.h>
 #include <sys/types.h>
 #include <sys/ioctl.h>
@@ -27,7 +29,7 @@ typedef struct
 } CONTEXT;
 
 static int listener_socket;
-
+static pid_t parent_pid;
 static void handle_read(struct ev_loop *loop, ev_io *w, int revents);
 static void handle_accept(struct ev_loop *loop, ev_io *w, int revents);
 
@@ -48,29 +50,19 @@ void *worker_thread(void *data)
   static ev_io worker_read;
   int count;
 
-  if (index == 1) {
-    sleep(5);
-    printf("I0 %d %d %p %d\n", listener_socket, index, &handler, handler.contexts.size());
-  }
-
   handler.handlers = (CONTEXT **) malloc(sizeof(CONTEXT *) * 8192);
   for (count = 0; count < 8192; count++) {
     handler.handlers[count] = (CONTEXT *) malloc(sizeof(CONTEXT));
-    if (count == 0) printf(" PPP : %d %p\n", getpid(), handler.handlers[count]);
     handler.handlers[count]->id = count;
     handler.contexts.push_back(handler.handlers[count]);
   }
-
-  printf("I0 %d %d %p %d\n", listener_socket, index, &handler, handler.contexts.size());
 
   loop = ev_loop_new(EVFLAG_AUTO);
 
   ev_io_init(&worker_read, handle_accept, listener_socket, EV_READ);
   ev_io_start(loop, &worker_read);
 
-  printf("Index : %p %d %d\n", loop, getpid(), index);
   ev_loop(loop, 0);
-  printf("Index End\n");
 
   exit(0);
 }
@@ -83,7 +75,7 @@ int create_server_socket()
 
   listenfd = socket(AF_INET, SOCK_STREAM, 0);
   serv_addr.sin_family = AF_INET; 
-  serv_addr.sin_port   = htons(8085); 
+  serv_addr.sin_port   = htons(8080); 
   serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); 
 
   arg = fcntl(listenfd, F_GETFL, NULL);
@@ -97,16 +89,26 @@ int create_server_socket()
   return listenfd;
 }
 
+void dump_buffer(const char *buf, int size)
+{
+  for (int count = 0; count < size; count++) {
+    printf("0x%02X, ", buf[count] & 0x00FF);
+    if ((count % 32) == 31) printf("\n");
+  }
+  printf("\n");
+}
+
 static void handle_read(struct ev_loop *loop, ev_io *w, int revents)
 {
   CONTEXT *c = (CONTEXT *) w->data;
 
   if (c != NULL) {
     c->read = recv(w->fd, c->rbuffer, sizeof(c->rbuffer), 0);
-    printf("Handle Read : %d %p %d %d %p\n", c->id, loop, getpid(), c->read, c);
     if (c->read == 0) {
       ev_io_stop(loop, w);
       handler.contexts.push_back(c);
+    } else {
+      dump_buffer(c->rbuffer, c->read);
     }
   }
 }
@@ -115,8 +117,6 @@ static void handle_write(struct ev_loop *loop, ev_io *w, int revents)
 {
   char buffer[8192];
   int read;
-
-  printf("Handle Write : %p %d %d \n", loop, getpid(), read);
 }
 
 static void handle_accept(struct ev_loop *loop, ev_io *w, int revents)
@@ -125,25 +125,14 @@ static void handle_accept(struct ev_loop *loop, ev_io *w, int revents)
   int clientfd;
   socklen_t sl = sizeof(addr);
 
-  printf("EV Socket Accept \n");
   clientfd = accept(w->fd, (struct sockaddr *) &addr, &sl);
   if (clientfd == -1) return;
-
-  printf("clientfd : %d %p\n", clientfd, loop);
-
-  printf("CONTEXT : %d\n", handler.handlers[0]->id);
-  printf("CONTEXT : %d\n", handler.handlers[1]->id);
-  printf("CONTEXT : %d\n", handler.handlers[2]->id);
-  printf("CONTEXT : %d\n", handler.handlers[3]->id);
-  printf("CONTEXT Size : %d\n", handler.contexts.size());
-
 
   CONTEXT *context = NULL;
 
   if (handler.contexts.size() > 0) {
     context = handler.contexts.front();
     handler.contexts.pop_front();
-    printf("C : %p %d\n", context, context->id);
   }
 
   ev_io_init(&context->worker_read, handle_read, clientfd, EV_READ);
@@ -156,17 +145,34 @@ static void handle_accept(struct ev_loop *loop, ev_io *w, int revents)
 void start_server()
 {
   listener_socket = create_server_socket();
+}
 
-#if 0
-  loop = ev_default_loop(EVFLAG_AUTO);
+static void signal_terminate(int signo)
+{
+  if (getpid() == parent_pid) {
+    close(listener_socket);
+    printf("Parent Quit\n");
+  }
+ 
+  exit(0);
+}
 
-  ev_io_init(&listener, handle_accept, listener_socket, EV_READ);
-  ev_io_start(loop, &listener);
+void init_signals()
+{
+  struct sigaction sig;
 
-  ev_loop(loop, 0);
+  sig.sa_flags = 0;
+  sig.sa_handler = signal_terminate;
 
-  exit(1);
-#endif
+  if (sigaction(SIGINT, &sig, NULL) < 0) {
+    printf("SIGINT failed : %s\n", strerror(errno));
+    exit(0);
+  }
+  if (sigaction(SIGTERM, &sig, NULL) < 0) {
+    printf("SIGTERM failed : %s\n", strerror(errno));
+    exit(0);
+  }
+
 }
 
 int main(int argc, char **argv)
@@ -175,6 +181,10 @@ int main(int argc, char **argv)
   int count;
 
   start_server();
+
+  init_signals();
+
+  parent_pid = getpid();
 
   for (count = 0; count < 2; count++) {
     pid = fork();
